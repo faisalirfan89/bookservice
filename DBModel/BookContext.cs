c17b2b8a2afa1b1e18d253e55248d63b1d9de7a7
Faisal Irfan <faisal.irfan#exxat.com>
2021-06-16 13:00:22 +0530

    changes

---
+++ DBModel/BookContext.cs
@@ -0,0 +1,22 @@
+﻿using BookApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.DBModel
+{
+    public class BookContext: DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+            DataGenerator.Initialize(this);
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
+
+
---
+++ DBModel/DataGenerator.cs
@@ -0,0 +1,52 @@
+﻿using BookApi.DBModel;
+using BookApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+public class DataGenerator
+{
+    public static void Initialize(BookContext context)
+    {
+
+            // Look for any board games.
+            if (context.Books.Any())
+            {
+                return;   // Data was already seeded
+            }
+
+            context.Books.AddRange(
+                new Book
+                {
+                    Id = 1,
+                    Name = "Candy Land",
+                    PublishYear = "2010",
+                    Author = "Unknown"
+                },
+                new Book
+                {
+                    Id = 2,
+                    Name = "Children of Heaven",
+                    PublishYear = "2015",
+                    Author = "Unknown"
+                },
+                new Book
+                {
+                    Id = 3,
+                    Name = "3 Ediots",
+                    PublishYear = "2008",
+                    Author = "Unknown"
+                },
+                new Book
+                {
+                    Id = 4,
+                    Name = "Candy Land",
+                    PublishYear = "2010",
+                    Author = "Unknown"
+                }
+                );
+
+            context.SaveChanges();
+    }
+}
